d1288c4537030627dbcc28303635159a783ec222
asvolokin <102984668+asvolokin@users.noreply.github.com>
2022-04-21 02:54:20 -0400

    Commit 3

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 from wordlists import WordLists
-
+from itertools import permutations
+from fileinput import FileInput
 
 class Checker:
     @staticmethod
@@ -23,4 +24,87 @@ class Checker:
                 k = word.replace(r, '')
                 if k in noun:
                     f.remove(word)
+
+    @staticmethod
+    def permutations_of_word(wrong_words):
+        dictionary = {}
+        WordLists.dictionary(dictionary)
+        poss = []
+        sugg = []
+        perm = permutations(wrong_words)
+        for items in list(perm):
+            poss.append(items)
+
+        possibilities = []
+
+        for elements in poss:
+            attempts = ''
+            for items in elements:
+                attempts += items
+            possibilities.append(attempts)
+
+        for items in possibilities:
+            if items in dictionary and items not in sugg:
+                sugg.append(items)
+
+        return sugg
+
+    @staticmethod
+    def double_to_single_letters(wrong_words):
+        dictionary = {}
+        WordLists.dictionary(dictionary)
+        sugg = []
+        poss = []
+
+        element = list(wrong_words)
+        for i in range(len(element)-1):
+            if element[i] == element[i - 1]:
+                del element[i]
+                poss.append(element)
+
+        possibilities = []
+
+        for elements in poss:
+            attempts = ''
+            for items in elements:
+                attempts += items
+            possibilities.append(attempts)
+
+        for items in possibilities:
+            if items in dictionary and items not in sugg:
+                sugg.append(items)
+
+        return sugg
+
+    @staticmethod
+    def single_letters_to_double_letters(wrong_words):
+        dictionary = {}
+        WordLists.dictionary(dictionary)
+        sugg = []
+        poss = []
+        element = str(wrong_words)
+        for i in range(len(element) - 1):
+            possibility = element[:i] + element[i] + element[i:]
+            poss.append(possibility)
+
+        for items in poss:
+            if items in dictionary and items not in sugg:
+                sugg.append(items)
+
+        return sugg
+
+    @staticmethod
+    def fix_text(wrong_words, suggestions, original, index, loop):
+        file = True
+        while file:
+
+            if index > len(suggestions) -1:
+                index = int(input('Invalid index, please try again'))
+            elif suggestions[index] == 'None of these options':
+                return original
+            elif index < len(suggestions) -1:
+                original = original.replace(wrong_words[loop], suggestions[index])
+                file = False
+
+        return original
 
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import time
 from checker import Checker
 from wordlists import WordLists
 
-tic = time.perf_counter()
 dictionary = {}
 
 
-def text(f):
-    f = open(f, encoding='utf8')
-    contents = f.read()
-    f.close()
+def Spell_Check(f):
+    with open(f, encoding='utf8') as fi:
+        contents = fi.read()
     spelling_errors = []
+    orig_content = contents
     contents = contents.lower()
+    print('Your original text is:\n', orig_content)
     WordLists.dictionary(dictionary)
     for p in """,.<>?/;:"[]{}|=_+)(*&^%$#@!`~""":  # gets rid of all punctuation
         contents = contents.replace(p, '')
@@ -23,10 +22,22 @@ def text(f):
             spelling_errors.append(word)
     Checker.possessive_nouns(spelling_errors)
     Checker.plural_possessive_nouns(spelling_errors)
-    return spelling_errors
+    loop_num = 0
+    for word in spelling_errors:
+        print('\nThe word', word, 'is spelled incorrectly\n')
+        lst1 = Checker.permutations_of_word(word)
+        lst2 = Checker.double_to_single_letters(word)
+        lst3 = Checker.single_letters_to_double_letters(word)
+        total_sugg = [*lst1, *lst2, *lst3, "None of these options"]
+        print(total_sugg)
+        ind = int(input('Please indicate the word you would like to replace the original '
+                      'with by the number position in the list: ')) - 1
+        orig_content = Checker.fix_text(spelling_errors, total_sugg, orig_content, ind, loop_num)
+        loop_num += 1
+    with open(f, 'w+', encoding='utf8') as fi:
+        fi.write(orig_content)
+    print('\nThe final text in your file is:\n',orig_content)
+    return
 
 
-toc = time.perf_counter()
-
-print(toc-tic)
-print(text('test.txt'))
+print(Spell_Check('test.txt'))
